754b2ddc3bd64eeee71c79bd9a9a7b9139411324
Sirjamo1 <jam_how@hotmail.com>
2023-03-31 12:19:35 +1000

    added more to animation splash screen

--- main.js
+++ main.js
@@ -6,8 +6,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const splashPage = document.querySelector(".splash-page");
 const carousel = document.querySelector(".carousel");
+const splashR = document.querySelector(".splash-r")
 
 const splashButton = document.getElementsByTagName("button")
+const neonRedText = document.querySelector(".neon-red-text")
+
 splashButton[0].addEventListener("click", () => {
- splashPage.style.display = "none";
+neonRedText.style.animation = "none"
+splashR.style.animation = "blackout 3s forwards"
+setTimeout(() => {
+    splashPage.style.display = "none";
+}, 3500);
+
 })
